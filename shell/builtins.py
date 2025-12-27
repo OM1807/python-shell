61afa943cf3532c40097e113a39f7cb1e8337140
OM1807 <omakiwate2408@gmail.com>
2025-12-27 13:08:12 +0530

    Added builtins logic

---
+++ shell/builtins.py
@@ -0,0 +1,27 @@
+BUILTINS = ["exit", "echo", "type"]
+
+def is_builtin(command):
+    return command in BUILTINS
+
+
+def execute_builtin(command, args):
+   
+    if command == "exit":
+        return "exit"
+
+    elif command == "echo":
+        print(" ".join(args))
+        return True
+
+    elif command == "type":
+        if not args:
+            return True
+
+        target = args[0]
+        if target in BUILTINS:
+            print(f"{target} is a shell builtin")
+        else:
+            print(f"{target}: not found")
+        return True
+
+    return False
