15619cb159839b923544de2b0c8e91bab13a41b0
OM1807 <omakiwate2408@gmail.com>
2025-12-27 13:33:09 +0530

    Redirection parsing

--- shell/parser.py
+++ shell/parser.py
@@ -1,6 +1,42 @@
-def parse(cmd):
-    cmd = cmd.split()
-    command = cmd[0]
-    arguments = cmd[1:]
+def parse(command_line):
+    tokens = command_line.split()
 
-    return command, arguments
+    command = None
+    args = []
+
+    stdin = None
+    stdout = None
+    append = False
+
+    i = 0
+    while i < len(tokens):
+        token = tokens[i]
+
+        if token == ">":
+            stdout = tokens[i + 1]
+            append = False
+            i += 2
+
+        elif token == ">>":
+            stdout = tokens[i + 1]
+            append = True
+            i += 2
+
+        elif token == "<":
+            stdin = tokens[i + 1]
+            i += 2
+
+        else:
+            if command is None:
+                command = token
+            else:
+                args.append(token)
+            i += 1
+
+    return {
+        "command": command,
+        "args": args,
+        "stdin": stdin,
+        "stdout": stdout,
+        "append": append
+    }
